70046501ebcaa5021aa92d070a494031ef6d7231
Aditya-Gupta1 <guptaaditya008@gmail.com>
2021-11-23 10:08:05 +0530

    feature: update customer information
    Note: Only customer address and phone no. can be updated.

--- seller.py
+++ seller.py
@@ -1,9 +1,8 @@
 from typing import List
-from product_information import product_information
 
 class seller:
     
-    def __init__(self, name: str, email: str, phone_no: int, inventory: List[product_information] = None, rating: int = 0) -> None:
+    def __init__(self, name: str, email: str, phone_no: int, inventory: List[dict] = None, rating: int = 0) -> None:
         self.name = name
         self.email = email
         self.phone_no = phone_no
